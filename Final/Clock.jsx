92251d4b35ef31300a4bd244ca10bbba2eb0c752
M Awais <khanmowais5@gmail.com>
2025-03-25 10:04:24 +0500

    added

---
+++ Final/Clock.jsx
@@ -0,0 +1,21 @@
+
+import { useEffect, useState } from "react";
+
+function Clock(){
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [count, setCount] = useState(20);
+    useEffect(() => {
+        
+        console.log('useEffect called')
+        setTime(new Date().toLocaleTimeString())
+        return () => {
+            console.log('Unmounting')
+        }
+    }, [])
+    return (
+        <div>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            <button className="bg-[#000000] text-white p-2 rounded-lg" onClick={() => setCount(count + 1)}>Change Time</button>
+        </div>
+    );
+    export default Clock
